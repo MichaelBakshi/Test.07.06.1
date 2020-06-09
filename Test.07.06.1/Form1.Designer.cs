f1c41872547cea98f06e95b373a3e270d1a66923
Michael Bakshi <michael.bakshi83@gmail.com>
2020-06-09 21:34:22 +0300

    Add project files.

---
+++ Test.07.06.1/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Test._07._06._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(112, 178);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(154, 20);
+            this.txtBox.TabIndex = 0;
+            this.txtBox.TextChanged += new System.EventHandler(this.txtBox_TextChanged);
+            // 
+            // lbl
+            // 
+            this.lbl.Location = new System.Drawing.Point(423, 178);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(299, 56);
+            this.lbl.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.txtBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Label lbl;
+    }
+}
+
---
+++ Test.07.06.1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test._07._06._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtBox_TextChanged(object sender, EventArgs e)
+        {
+            int input = Convert.ToInt32(txtBox.Text);
+
+            if (input == 0)
+            {
+                lbl.Text = "Zero";
+            }
+            else if (input < 0)
+            {
+                lbl.Text = "Negative";
+
+            }
+            else if (input > 0)
+            {
+                lbl.Text = "Positive";
+
+            }
+            
+            else if (txtBox.Text == null)
+            {
+                lbl.Text = "Nan";
+            }
+        }
+    }
+}
